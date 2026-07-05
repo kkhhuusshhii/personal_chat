6464d878296aa386170f91a307477ef0e3d37261
kkhhuusshhii <104735323+kkhhuusshhii@users.noreply.github.com>
2026-07-05 21:55:27 +0530

    Delete app directory

--- app/page.js
+++
@@ -1,7 +0,0 @@
-import ChatApp from "@/components/ChatApp";
-import { listPersonas } from "@/lib/personas";
-
-export default function Home() {
-  const personas = listPersonas();
-  return <ChatApp personas={personas} />;
-}
